d9dcd96156eb62a472b5981e9b2a0eb84ecabb15
Matt Holmes <matthew.allen.holmes@gmail.com>
2013-07-28 14:43:32 -0700

    work on the server side parser

---
+++ lib/combobuilder/parser.rb
@@ -0,0 +1,116 @@
+module ComboBuilder
+
+  module ComboNode
+    attr_accessor :type
+  end
+
+  class ErrorNode
+    include ComboNode
+
+    attr_accessor :message
+
+    def initialize(msg)
+      @type = :error
+      @message = msg
+    end
+  end
+
+  class SequenceNode
+    include ComboNode
+
+    attr_accessor :parts, :modifier
+
+    def initialize
+      @type = :sequence
+      @parts = []
+    end
+
+    def has_modifier?
+      !modifier.nil?
+    end
+  end
+
+  module Parser
+    def parse(input_scheme, text)
+      ctx = Parser::Context.new(input_scheme, text)
+      parts = text.split(' ')
+      nodes = []
+      parts.each do |part|
+        if part =~ /.*\..*/
+          nodes << parse_modifier_part(ctx, part)
+        else
+          nodes << parse_any_part(ctx, part)
+        end
+      end
+
+      nodes
+    end
+
+    private
+
+    class Context
+      attr_accessor :input_scheme, :text, :button_list, :any_list
+
+      def initialize(input_scheme, text)
+        @input_scheme = input_scheme
+        @text = text
+
+        @button_list = build_button_list
+        @any_list = build_any_list
+      end
+
+      private
+
+      def build_button_list
+        @input_scheme.buttons.sort {|a, b| a.length <=> b.length }
+      end
+
+      def build_any_list
+        any = @input_scheme.buttons.concat(@input_scheme.motions)
+        any.sort {|a, b| a.length <=> b.length }
+      end
+    end
+
+    def parse_modifier_part(ctx, part)
+      parts = part.split('.')
+      modifier = parts[0]
+      sequence = parts[1]
+
+      if !ctx.input_scheme.modifiers.contains modifier
+        return ErrorNode.new("Unknown modifier: #{modifier}")
+      end
+
+      node = SequenceNode.new
+      node.modifier = modifier``
+      return parse_sequence(sequence, ctx.button_list, node)
+    end
+
+    def parse_any_part(ctx, part)
+      parse_sequence(part, ctx.any_list, SequenceNode.new)
+    end
+
+    def parse_sequence(part, list, node)
+      loop do
+        found = false
+
+        list.each do |btn|
+          if part.starts_with?(btn)
+            found = true
+            part = part.slice(btn.length, part.length - btn.length)
+            node.parts << btn
+          end
+        end
+
+        break if !found || part.length == 0
+      end
+
+      if part.length > 0
+        return ErrorNode.new("Unknown input in sequence: #{part}")
+      end
+
+      node
+    end
+
+  end
+
+end
